932a21d8c0c5be1574363560f8f4498ff84bcd1b
ebenner <ebennerit@gmail.com>
2021-06-15 13:55:37 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # vultr-marketplace-scripts
 A series of helper scripts meant to make making Vultr Marketplace apps easier on the end user
+
+# vultr-tools.sh
+This script is intended to be put on an image to facilitate both building and post deploy. It contains
+a series of function to make both easier and reduce complexity and maintenance of your app.
+
+```bash
+#!/bin/bash
+
+# Fail out on errors, you should definitely use this if possible
+# This prevents silent failures from producing failed images
+set -eo pipefail
+
+# Include the file into your environment
+. /opt/myapp/vultr-tools.sh
+
+# Install packages via apt without worrying about apt locks
+apt_safe nano vim wget
+
+# Update repos via apt without worrying about apt locks
+apt_update_safe
+
+# Update packages via apt without worrying about apt locks
+apt_upgrade_safe
+
+# Clean packages via apt without worrying about apt locks
+apt_clean_safe
+
+# Get the hostname for this instance
+HOSTNAME=$(get_hostname)
+
+# Get the root password for this instance, this will be a hash
+ROOTPASS=$(get_root_password)
+
+# Get the userdata for this instance
+USERDATA=$(get_userdata)
+
+# Get the SSH Keys for this instance
+SSHKEYS=$(get_sshkeys)
+
+# This is to get a variable and set it. The variable name is what will be retrieved and set
+get_var site_password
+echo ${site_password}
+
+# This will install cloud-init. Note this only works for RHEL based, Debian based, and Ubuntu based distros!
+# Acceptable options are latest,nightly, generally you want to use latest
+install_cloud_init latest
+
+# This cleans the system for final release
+# If you set a pipefail, and you should of, it is safe to disable it for this line
+# Otherwise incompatible commands will cause a failure
+set +eo pipefail
+
+# This is the command that cleans the system
+clean_system
+
+set -eo pipefail
+```
---
+++ vultr-tools.sh
@@ -0,0 +1,205 @@
+#!/bin/bash
+
+set -eo pipefail
+
+function apt_safe() {
+    RES=$(dpkg -i /dev/zero 2>&1 | grep "frontend lock" | wc -l)
+	while [ "$RES" -ne 0 ] ;
+	do
+		echo "Waiting for apt lock"
+		sleep 1
+		RES=$(dpkg -i /dev/zero 2>&1 | grep "frontend lock" | wc -l)
+	done
+
+    apt install -y $@
+}
+
+function apt_update_safe() {
+    RES=$(dpkg -i /dev/zero 2>&1 | grep "frontend lock" | wc -l)
+	while [ "$RES" -ne 0 ] ;
+	do
+		echo "Waiting for apt lock"
+		sleep 1
+		RES=$(dpkg -i /dev/zero 2>&1 | grep "frontend lock" | wc -l)
+	done
+
+    apt update -y
+}
+
+function apt_upgrade_safe() {
+    RES=$(dpkg -i /dev/zero 2>&1 | grep "frontend lock" | wc -l)
+	while [ "$RES" -ne 0 ] ;
+	do
+		echo "Waiting for apt lock"
+		sleep 1
+		RES=$(dpkg -i /dev/zero 2>&1 | grep "frontend lock" | wc -l)
+	done
+
+    apt upgrade -y
+}
+
+function apt_clean_safe() {
+    RES=$(dpkg -i /dev/zero 2>&1 | grep "frontend lock" | wc -l)
+	while [ "$RES" -ne 0 ] ;
+	do
+		echo "Waiting for apt lock"
+		sleep 1
+		RES=$(dpkg -i /dev/zero 2>&1 | grep "frontend lock" | wc -l)
+	done
+
+    apt autoremove -y
+
+    RES=$(dpkg -i /dev/zero 2>&1 | grep "frontend lock" | wc -l)
+	while [ "$RES" -ne 0 ] ;
+	do
+		echo "Waiting for apt lock"
+		sleep 1
+		RES=$(dpkg -i /dev/zero 2>&1 | grep "frontend lock" | wc -l)
+	done
+
+    apt autoclean -y
+}
+
+function get_hostname() {
+    HOSTNAME=$(curl --fail -s "http://169.254.169.254/latest/meta-data/hostname")
+    echo "${HOSTNAME}"
+}
+
+function get_root_password() {
+    ROOTPW=$(curl -H "Metadata-Token: vultr" --fail -s "http://169.254.169.254/v1/internal/root-password")
+    echo "${ROOTPW}"
+}
+
+function get_userdata() {
+    USERDATA=$(curl --fail -s "http://169.254.169.254/latest/user-data")
+    echo "${USERDATA}"
+}
+
+function get_sshkeys() {
+    KEYS=$(curl --fail -s "http://169.254.169.254/current/ssh-keys")
+    echo "${KEYS}"
+}
+
+function get_var() {
+    local val="$(curl --fail -s  http://169.254.169.254//v1/internal/app-${1} 2>/dev/null)"
+
+	local __result=$1
+	eval $__result="'${val}'"
+}
+
+function install_cloud_init
+{
+	if [ -f /etc/redhat-release ]; then
+        BUILD="rhel"
+		DIST="rpm"
+    fi
+
+    if [ "$(grep -c "ID=ubuntu" /etc/os-release)" != "0" ]; then
+        BUILD="universal"
+		DIST="deb";
+    fi
+
+    if [ "$(grep -c "ID=debian" /etc/os-release)" != "0" ]; then
+        BUILD="debian10"
+		DIST="deb";
+    fi
+
+    if [ "${DIST}" == "" ]; then
+        echo "Undetected OS, please install from source!"
+        exit 255
+    fi
+
+
+	RELEASE=$1
+
+	if [ "${RELEASE}" == "" ]; then
+		RELEASE="latest"
+	fi
+
+	if [ "${RELEASE}" != "latest" ] && [ "${RELEASE}" != "nightly" ]; then
+		echo "${RELEASE} is an invalid release option. Allowed: latest, nightly"
+        exit 255
+	fi
+
+	if [ "${DIST}" == "rpm" ]; then
+		DIST="rpm"
+	elif [ "${DIST}" == "deb" ]; then
+		DIST="deb";
+	fi
+
+	echo "nameserver 8.8.8.8" > /etc/resolv.conf
+	wget https://ewr1.vultrobjects.com/cloud_init_beta/cloud-init_${BUILD}_${RELEASE}.${DIST} -O /tmp/cloud-init_${BUILD}_${RELEASE}.${DIST}
+
+	if [ "${DIST}" == "rpm" ]; then
+		yum install -y /tmp/cloud-init_${BUILD}_${RELEASE}.${DIST}
+	elif [ "${DIST}" == "deb" ]; then
+		apt_safe /tmp/cloud-init_${BUILD}_${RELEASE}.${DIST}
+	fi
+
+	rm -f /tmp/cloud-init_${BUILD}_${RELEASE}.${DIST}
+}
+
+function clean_system() {
+
+    # Update and clean packages
+    if [ -f /etc/redhat-release ]; then
+        yum update -y
+        yum clean all
+    fi
+
+    if [ "$(grep -c "debian" /etc/os-release)" != "0" ]; then
+        apt_update_safe
+        apt_upgrade_safe
+        apt_clean_safe
+    fi
+
+    # Ensure temp exists
+    mkdir /tmp
+    chmod 1777 /tmp
+
+    # Clear temp
+    rm -rf /tmp/*
+    rm -rf /var/tmp/*
+
+    # Clear keys
+    rm -f /root/.ssh/authorized_keys /etc/ssh/*key*
+    touch /etc/ssh/revoked_keys
+    chmod 600 /etc/ssh/revoked_keys
+
+    # Clear logs
+    find /var/log -mtime -1 -type f -exec truncate -s 0 {} \;
+    rm -rf /var/log/*.gz
+    rm -rf /var/log/*.[0-9]
+    rm -rf /var/log/*.log
+    rm -rf /var/log/lastlog
+    rm -rf /var/log/wtmp
+
+    # Clear old Cloud-Init data
+    rm -rf /var/lib/cloud/instances/*
+
+    # Clear history
+    history -c
+    echo "" > /root/.bash_history
+
+    # Clear mloc
+    /usr/bin/updatedb
+
+    # Clear random seed
+    rm -f /var/lib/systemd/random-seed
+
+    # Clear machine ID
+    rm -f /etc/machine-id
+    touch /etc/machine-id
+
+    # Zero empty space
+    dd if=/dev/zero of=/zerofile
+    sync
+    rm -f /zerofile
+    sync
+
+    # Trim / if possible
+    fstrim /
+
+    # Clean Cloud-init
+    cloud-init clean
+}
